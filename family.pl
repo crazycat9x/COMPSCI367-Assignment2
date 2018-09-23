de1813789ff5e217a8cd773b114143513193a880
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-24 09:02:36 +1200

    Delete comment

--- family.pl
+++ family.pl
@@ -1,13 +1,5 @@
 :- dynamic female/1, male/1, childOf/2.
 
-% TEST SET
-% male(alex).
-% female(sophia).
-% childOf(sophia, tom).
-% childOf(may,tom).
-% childOf(john,alex).
-% childOf(john,sophia).
-
 motherOf(Mom, Child) :- female(Mom), childOf(Child, Mom).
 
 sisterOf(Sister, Person) :- female(Sister), childOf(Person,Parent), childOf(Sister, Parent).
