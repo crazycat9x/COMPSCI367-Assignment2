06f19fc718955c5b9d518c3d9ee699fca0f03310
Tiến Nguyễn Khắc <crazycat194hb@gmail.com>
2018-09-24 13:11:06 +1200

    Reformat

--- family.pl
+++ family.pl
@@ -1,15 +1,24 @@
-:- dynamic female/1, male/1, childOf/2.
+:- (dynamic female/1, male/1, childOf/2).
 
-motherOf(Mom, Child) :- female(Mom), childOf(Child, Mom).
+motherOf(Mom, Child) :-
+    female(Mom),
+    childOf(Child, Mom).
 
-sisterOf(Sister, Person) :- female(Sister), childOf(Person,Parent), childOf(Sister, Parent).
+sisterOf(Sister, Person) :-
+    female(Sister),
+    childOf(Person, Parent),
+    childOf(Sister, Parent).
 
-ancestorOf(Anc,Pre) :-
-  childOf(Pre,Anc);
-  childOf(X,Anc), ancestorOf(X,Pre).
+ancestorOf(Anc, Pre) :-
+    (   childOf(Pre, Anc)
+    ;   childOf(X, Anc),
+        ancestorOf(X, Pre)
+    ).
 
 inSameTree(Person1, Person2) :-
-  Person1 == Person2;
-  ancestorOf(Person2, Person1);
-  ancestorOf(Person1, Person2);
-  ancestorOf(Anc,Person1), ancestorOf(Anc,Person2).
+    (   Person1==Person2
+    ;   ancestorOf(Person2, Person1)
+    ;   ancestorOf(Person1, Person2)
+    ;   ancestorOf(Anc, Person1),
+        ancestorOf(Anc, Person2)
+    ).
